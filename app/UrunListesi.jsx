69a4daa0e6163ab100e008634c1f6ac13d67f877
Mehmet Elbeyli <mehmetelbeyli88@gmail.com>
2023-07-05 13:15:35 +0300

    Lenght Hatası Çözüldü

--- app/UrunListesi.jsx
+++ app/UrunListesi.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectUrunler } from "./GlobalState/Features/urunler/urunSlice";
 function UrunListesi() {
-  const tumUrunler = useSelector(selectUrunler);
+  const tumUrunler = useSelector(selectUrunler) || [];
   return (
     <div className="flex w-full p-6 overflow-x-auto shadow-md rounded-md sm:rounded-lg">
       <table className="w-full text-sm text-left bg-gray-700 text-gray-500 dark:text-gray-400 rounded-sm">
